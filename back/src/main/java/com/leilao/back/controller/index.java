8e9606634944d62bb86cf450737ac93aa616ea92
GregoryVinicius <gregoryviniciuscla@gmail.com>
2024-09-23 22:23:57 -0300

    Começando o backend do projeto

---
+++ back/src/main/java/com/leilao/back/controller/index.java
@@ -0,0 +1,20 @@
+package com.leilao.back.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class index {
+    @GetMapping
+    public String index() {
+        return "Hello World Spring";
+    }
+
+    @PostMapping
+    public String save() {
+        return "Sucesso";
+    }
+}
